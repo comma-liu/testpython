6668fe38c176104b916cb936d5535efc9851cb4c
comma-liu <72338181+comma-liu@users.noreply.github.com>
2024-06-27 17:59:35 +0800

    Add files via upload

--- test2.py
+++ test2.py
@@ -5,43 +5,10 @@ import requests
 import openpyxl
 from openpyxl import load_workbook
 
-def main():
-    # 通过requests模块的get函数获取网络资源
-    url = "https://ssr1.scrape.center/"
-    resp = requests.get(url)
-    bs_resp = bs(resp.content, features='html.parser')
-    resdict = {}
-    
-    testbs2 = bs_resp.find_all(attrs={"class":"categories"})
-    testbs4 = bs_resp.find_all(attrs={"class":"score m-t-md m-b-n-sm"})
-    for i in range(0,10):
-            testbs1 = bs_resp.find_all(attrs={"href": f"/detail/{i+1}"})
-            resdict[i] = [testbs1[1].h2.text]
-            #print(testbs1[1].h2.text) 片名
-            for j in testbs2[i].find_all("span"):            
-    #           print(j.text);#种类
-                resdict[i].append(j.text)
-            resdict[i].append(testbs4[i].text)
-            #print(testbs4[i].text)#评分   
-            
-    #class="m-v-sm info"信息
-    testbs3 = bs_resp.find_all(attrs={"class":"m-v-sm info"}) 
-    for i in range(0,20,2):
-        for m in testbs3[i].find_all("span"):
-    #        print(m.text)#地区
-            mstr += m.text
-        resdict[i].append(mstr)
-        for n in testbs3[i+1].find_all("span"):
-    #        print(n.text)#时间
-        resdict[i].append(n.text)
-    #    print("======")
-    print(resdict)
-    
 
-        
-    
-        
-"""     write_wb = openpyxl.Workbook()
+def to_xlsx(filename, page):
+    resdict = get_html(f"https://ssr1.scrape.center/page/{page}")
+    write_wb = openpyxl.Workbook()
     write_sheet = write_wb.active
     print('开始写入excel')
     # 表头信息
@@ -55,28 +22,75 @@ def main():
     # 表头写入Excel中
     write_sheet.append(cxcel_title)
     # 循环写入旧Excel数据
-    for row in sheet.rows:
+    for row in resdict:
         # 行信息
-        title_1 = row[0].value
-        title_2 = row[1].value
-        title_3 = row[2].value
-        title_4 = row[3].value
-        title_5 = row[4].value
-        title_6 = row[5].value
+        title_1 = resdict[row][0]
+        title_2 = resdict[row][1]
+        title_3 = resdict[row][2]
+        title_4 = resdict[row][3]
+        title_5 = resdict[row][4]
         excel_row = [
             title_1,
             title_2,
             title_3,
             title_4,
             title_5,
-            title_6
         ]
         # 写入Excel中
         write_sheet.append(excel_row)
     # 生成excel文件
-    write_wb.save('new_test.xlsx')
-    print('写入excel完成！') """
+
+    write_wb.save(filename)
+    print('写入excel完成！') 
+
+
+def get_html(url):
+    # url = "https://ssr1.scrape.center/"
+    resp = requests.get(url)
+    bs_resp = bs(resp.content, features='html.parser')
+    resdict = {}
+    movie_title = []
+    movie_caro = []
+    movie_score = []
+    movie_area = []
+    movie_time = []
     
+    testbs2 = bs_resp.find_all(attrs={"class":"categories"})
+    testbs4 = bs_resp.find_all(attrs={"class":"score m-t-md m-b-n-sm"})
+    for i in range(0,10):
+            testbs1 = bs_resp.find_all(attrs={"href": f"/detail/{i+1}"})
+            movie_title.append(testbs1[1].h2.text)
+            caro = ' '
+            for j in testbs2[i].find_all("span"):            
+                caro += j.text
+                caro += ' '
+            movie_caro.append(caro)
+            movie_score.append(float(testbs4[i].text))         
+
+    testbs3 = bs_resp.find_all(attrs={"class":"m-v-sm info"}) 
+    for i in range(0,20,2):
+        area = ' '
+        time = ' '
+        for m in testbs3[i].find_all("span"):
+            area += m.text
+            area += ' '
+        movie_area.append(area)
+        for n in testbs3[i+1].find_all("span"):
+
+             time += n.text
+             time += ' '
+        movie_time.append(time)
+    for i in range(0,10):    
+        resdict[i] = (movie_title[i],movie_caro[i],movie_area[i],movie_time[i],movie_score[i])
+
+    return resdict
+
+def main():
+    #to_xlsx('第一页.xlsx',1)  
+    #to_xlsx('第二页.xlsx',2)  
+    resp = requests.get("https://ssr1.scrape.center/page/2")
+    bs_resp = bs(resp.content, features='html.parser')
+    print(bs_resp)
 
 if __name__ == '__main__':
     main()
